33e9f7d46f79000e85cd0c75fa0a9b7a54e0da8f
RachelleClt <colat.rachelle1@gmail.com>
2021-06-17 10:21:07 -0400

    Premier commit : entités et relation

---
+++ src/Entity/Event.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $startdate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $enddate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $place;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phase;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbteam;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="events")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $User;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartdate(): ?string
+    {
+        return $this->startdate;
+    }
+
+    public function setStartdate(string $startdate): self
+    {
+        $this->startdate = $startdate;
+
+        return $this;
+    }
+
+    public function getEnddate(): ?string
+    {
+        return $this->enddate;
+    }
+
+    public function setEnddate(string $enddate): self
+    {
+        $this->enddate = $enddate;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getPhase(): ?string
+    {
+        return $this->phase;
+    }
+
+    public function setPhase(string $phase): self
+    {
+        $this->phase = $phase;
+
+        return $this;
+    }
+
+    public function getNbteam(): ?int
+    {
+        return $this->nbteam;
+    }
+
+    public function setNbteam(int $nbteam): self
+    {
+        $this->nbteam = $nbteam;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): self
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+}
